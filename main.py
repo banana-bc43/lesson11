8fd2b54cb102304edbed42e0661656b0bb88187b
吉岡祐翔 <jigangyuuto5@gmail.com>
2025-09-07 13:04:48 +0900

    完成品です。

--- main.py
+++ main.py
@@ -1,39 +1,80 @@
 import streamlit as st
 import pandas as pd
 
+# 言語選択
+language = st.sidebar.selectbox('言語を選択してください / Choose a language', ['日本語', 'English'])
+
+# 言語によるテキストの設定
+if language == '日本語':
+    title = '📅 スケジュール表'
+    intro_text = '予定を入力して、スケジュールを作成しよう！'
+    time_label = '時間 (半角で入力: 例 09:00)'
+    task_label = '予定を入力'
+    add_button = '予定を追加'
+    success_add = '新しい予定が追加されたよ！'
+    error_message = '時間と予定の両方を入力してね！'
+    select_label = '削除/修正する予定を選んでね'
+    edit_time_label = '修正する時間'
+    edit_task_label = '修正する予定'
+    edit_button = '選択した予定を修正'
+    delete_button = '選択した予定を削除'
+    success_edit = '選択した予定が修正されたよ！'
+    success_delete = '選択した予定が削除されたよ！'
+    no_schedule = 'まだ予定が追加されていないよ。'
+    sleep_message = "<h2 style='text-align: center; color: green;'>That nice! <br>いいルーティーンができたね！<br>本日もお疲れ様です。</h2>"
+else:
+    title = '📅 Schedule'
+    intro_text = 'Enter your plans and create a schedule!'
+    time_label = 'Time (Enter in half-width: e.g., 09:00)'
+    task_label = 'Enter your task'
+    add_button = 'Add Task'
+    success_add = 'New task added!'
+    error_message = 'Please enter both time and task!'
+    select_label = 'Select a task to delete/edit'
+    edit_time_label = 'Edit time'
+    edit_task_label = 'Edit task'
+    edit_button = 'Edit selected task'
+    delete_button = 'Delete selected task'
+    success_edit = 'Selected task edited!'
+    success_delete = 'Selected task deleted!'
+    no_schedule = 'No tasks added yet.'
+    sleep_message = "<h2 style='text-align: center; color: green;'>That nice! <br>Great routine!<br>Good job today.</h2>"
+
 # セッションステートを使ってスケジュールデータを保持
 if 'schedule_data' not in st.session_state:
     st.session_state['schedule_data'] = []
 
 # ストリームリットで表示
-st.title('📅 スケジュール表')
-st.write('今日の予定を入力して、スケジュールを作成しましょう！')
+st.title(title)
+st.write(intro_text)
 
 # 新しい予定の入力
-new_time = st.text_input('時間を入力してください (例: 09:00)', key='new_time')
-new_task = st.text_input('予定を入力してください', key='new_task')
+new_time = st.text_input(time_label, key='new_time')
+new_task = st.text_input(task_label, key='new_task')
 
 # 全角を半角に変換する関数
 def to_half_width(input_str):
-    return input_str.translate(str.maketrans('０１２３４５６７８９：', '0123456789:'))
+    return input_str.translate(str.maketrans('１２３４５６７８９：', '123456789:'))
 
 # 追加ボタン
-if st.button('予定を追加'):
+if st.button(add_button):
     # 時間を半角に変換
     new_time = to_half_width(new_time)
     
     if new_time and new_task:
         # 新しい予定をセッションステートに追加
-        st.session_state['schedule_data'].append({'時間': new_time, '予定': new_task})
-        st.success('新しい予定が追加されました！')
-
+        if language =="日本語":
+            st.session_state['schedule_data'].append({'時間': new_time, '予定': new_task })
+        else :
+            st.session_state['schedule_data'].append({'time': new_time, 'schedule': new_task })
+        st.success(success_add)
         # 「寝る」や「就寝」というキーワードが含まれているかチェック
-        if '寝る' in new_task or '就寝' in new_task:
+        if '寝る' in new_task or '就寝' in new_task or 'Sleep' in new_task :
             st.balloons()
             # メッセージを中央に表示
-            st.markdown("<h2 style='text-align: center; color: green;'>That nice!<br>いいルーティーンができたね！本日もお疲れ様です。</h2>", unsafe_allow_html=True)
+            st.markdown(sleep_message, unsafe_allow_html=True)
     else:
-        st.error('時間と予定の両方を入力してください。')
+        st.error(error_message)
 
 # データフレームを作成
 schedule_df = pd.DataFrame(st.session_state['schedule_data'])
@@ -43,16 +84,20 @@ if not schedule_df.empty:
     st.table(schedule_df)
 
     # 削除または修正する行を選択
-    selected_index = st.selectbox('削除または修正する予定を選んでください', range(len(schedule_df)), format_func=lambda x: f"{schedule_df.iloc[x]['時間']} - {schedule_df.iloc[x]['予定']}")
+    selected_index = st.selectbox(select_label, range(len(schedule_df)), format_func=lambda x: f"{schedule_df.iloc[x]['時間']} - {schedule_df.iloc[x]['予定']}", index=0)
 
-    # 削除ボタン
-    if st.button('選択した予定を削除'):
-        st.session_state['schedule_data'].pop(selected_index)
-        st.success('選択した予定が削除されました！')
+    # 選択した予定の編集
+    edit_time = st.text_input(edit_time_label, value=schedule_df.iloc[selected_index]['時間'])
+    edit_task = st.text_input(edit_task_label, value=schedule_df.iloc[selected_index]['予定'])
 
     # 修正ボタン
-    if st.button('選択した予定を修正'):
-        st.session_state['schedule_data'][selected_index] = {'時間': new_time, '予定': new_task}
-        st.success('選択した予定が修正されました！')
+    if st.button(edit_button):
+        st.session_state['schedule_data'][selected_index] = {'時間': to_half_width(edit_time), '予定': edit_task}
+        st.success(success_edit)
+
+    # 削除ボタン
+    if st.button(delete_button):
+        st.session_state['schedule_data'].pop(selected_index)
+        st.success(success_delete)
 else:
-    st.write('まだ予定が追加されていません。')
+    st.write(no_schedule)
